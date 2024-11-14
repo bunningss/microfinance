1efe2631dcd4dcc2fc97676de8342618991f6ac3
tayef mahmud <mahmudovi183@gmail.com>
2024-11-14 16:51:17 +0600

    print salary table

--- src/app/(dashboard)/dashboard/expenses/salary-statements/page.jsx
+++ src/app/(dashboard)/dashboard/expenses/salary-statements/page.jsx
@@ -1,54 +1,13 @@
 import { Block } from "@/components/block";
+import { PaginationControls } from "@/components/pagination-controls";
+import { SalaryTable } from "@/components/salary-table";
 import { getData } from "@/utils/api-calls";
 
 async function Salaries() {
   const res = await getData("expenses/salary", 0);
   const salaries = res.response.payload;
 
-  return (
-    <table className="w-full">
-      <thead>
-        <tr className="text-center bg-input">
-          <th scope="col" className="p-2 border-r border-primary md:border-0">
-            Serial Number
-          </th>
-          <th scope="col" className="p-2 border-r border-primary md:border-0">
-            Name
-          </th>
-          <th scope="col" className="p-2 border-r border-primary md:border-0">
-            Month
-          </th>
-          <th scope="col" className="p-2 border-r border-primary md:border-0">
-            Date
-          </th>
-          <th scope="col" className="p-2 border-r border-primary md:border-0">
-            Amount
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {salaries?.map((salary, index) => (
-          <tr key={index} className="text-center even:bg-input">
-            <td className="p-2 border-r border-primary md:border-0">
-              {index + 1}
-            </td>
-            <th scope="row" className="p-2 border-r border-primary md:border-0">
-              {salary.staff?.name}
-            </th>
-            <td className="p-2 border-r border-primary md:border-0">
-              {salary?.month}
-            </td>
-            <td className="p-2 border-r border-primary md:border-0">
-              {salary.createdAt}
-            </td>
-            <td className="p-2 border-r border-primary md:border-0">
-              ৳ {salary.amount}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+  return <SalaryTable salaries={salaries} />;
 }
 
 export default function Page() {
@@ -56,6 +15,7 @@ export default function Page() {
     <div className="space-y-4">
       <Block title="salary statements" />
       <Salaries />
+      <PaginationControls />
     </div>
   );
 }
---
+++ src/components/salary-table.jsx
@@ -0,0 +1,63 @@
+"use client";
+import { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
+import { Button } from "./ui/button";
+
+export function SalaryTable({ salaries }) {
+  const contentRef = useRef(null);
+  const reactToPrintFn = useReactToPrint({ contentRef });
+
+  return (
+    <>
+      <Button onClick={reactToPrintFn}>Print Data</Button>
+      <table
+        className="w-full rounded-md border border-secondary print:mx-4"
+        ref={contentRef}
+      >
+        <thead>
+          <tr className="text-center bg-input">
+            <th scope="col" className="p-2 border-r border-primary md:border-0">
+              Serial Number
+            </th>
+            <th scope="col" className="p-2 border-r border-primary md:border-0">
+              Name
+            </th>
+            <th scope="col" className="p-2 border-r border-primary md:border-0">
+              Month
+            </th>
+            <th scope="col" className="p-2 border-r border-primary md:border-0">
+              Date
+            </th>
+            <th scope="col" className="p-2 border-r border-primary md:border-0">
+              Amount
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {salaries?.map((salary, index) => (
+            <tr key={index} className="text-center even:bg-secondary">
+              <td className="p-2 border-r border-primary md:border-0">
+                {index + 1}
+              </td>
+              <th
+                scope="row"
+                className="p-2 border-r border-primary md:border-0"
+              >
+                {salary.staff?.name}
+              </th>
+              <td className="p-2 border-r border-primary md:border-0">
+                {salary?.month}
+              </td>
+              <td className="p-2 border-r border-primary md:border-0">
+                {new Date(salary.createdAt).toDateString()}
+              </td>
+              <td className="p-2 border-r border-primary md:border-0">
+                ৳ {salary.amount}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
