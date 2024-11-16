433a8c8ab4b08540cee6b75bd705523716168118
tayef mahmud <mahmudovi183@gmail.com>
2024-11-16 21:41:05 +0600

    admin ver for member data fetch

--- src/app/api/members/[id]/route.js
+++ src/app/api/members/[id]/route.js
@@ -2,10 +2,18 @@ import Member from "@/lib/models/Member";
 import Savings from "@/lib/models/Savings";
 import { connectDb } from "@/lib/db/connectDb";
 import { NextResponse } from "next/server";
+import { verifyToken } from "@/utils/auth";
 
 export async function GET(request, { params }) {
   try {
+    const { error, id } = await verifyToken(request);
+    if (error)
+      return NextResponse.json({ msg: "আপনি অনুমোদিত নন।" }, { status: 401 });
+
     await connectDb();
+    const user = await Staff.findById(id);
+    if (user.role !== "admin")
+      return NextResponse.json({ msg: "আপনি অনুমোদিত নন।" }, { status: 401 });
 
     const member = await Member.findOne({ nidNumber: params.id }).populate(
       "savings"
