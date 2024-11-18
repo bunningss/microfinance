04cf46b568bb813f2c5466e1da2593a640c9903d
tayef mahmud <mahmudovi183@gmail.com>
2024-11-18 12:27:20 +0600

    select component mobile style

--- src/components/cards/savings-card.jsx
+++ src/components/cards/savings-card.jsx
@@ -33,7 +33,7 @@ export function SavingsCard({ data, installments }) {
           <span className="capitalize">
             সঞ্চয়ের ধরণ: <b>{data?.savingsType}</b>
           </span>
-          <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between flex-wrap lg:flex-nowrap">
             <span className="capitalize">
               {data?.savingsType} সঞ্চয় পরিমাণ:{" "}
               <span className="text-primary font-bold uppercase">
