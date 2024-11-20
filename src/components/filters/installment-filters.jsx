c6f7a7cb7dc094ad3495c0f73f06e12bf79d79e5
tayef mahmud <mahmudovi183@gmail.com>
2024-11-20 12:33:05 +0600

    send proper date for filtering installments

--- src/components/filters/installment-filters.jsx
+++ src/components/filters/installment-filters.jsx
@@ -22,7 +22,7 @@ export function InstallmentsFilters() {
     : new Date(Date.now()).toDateString();
 
   const handleSubmit = (data) => {
-    router.push(`?date=${new Date(data.date).toISOString()}`);
+    router.push(`?date=${new Date(data.date).toLocaleDateString()}`);
   };
 
   const handleClear = () => {
