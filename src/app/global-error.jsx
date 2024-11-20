9b559c200fd446ffe802674641dfbf50dffe8325
tayef mahmud <mahmudovi183@gmail.com>
2024-11-20 16:54:03 +0600

    error message

--- src/app/global-error.jsx
+++ src/app/global-error.jsx
@@ -1,11 +1,20 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
 export default function GlobalError({ error, reset }) {
+  const router = useRouter();
+
   return (
     <html>
       <body>
-        <main className="h-full w-full flex justify-center items-center">
-          <h2 className="text-3xl">Something went wrong.</h2>
+        <main className="h-screen w-screen flex justify-center items-center">
+          <h2 className="text-xl md:text-3xl text-center">
+            You do not have permission to view this page. Please contact
+            administrator.
+          </h2>
+          <Button onClick={() => router.back()}>Go Back</Button>
         </main>
       </body>
     </html>
