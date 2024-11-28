93bf5b04d31e65297de59f16bae65230742d7737
tayef mahmud <mahmudovi183@gmail.com>
2024-11-28 11:08:37 +0600

    unified expense table

--- src/components/expenses-table.jsx
+++ src/components/expenses-table.jsx
@@ -1,92 +1,50 @@
 "use client";
+
 import Link from "next/link";
-import { useRef } from "react";
-import { Button } from "./ui/button";
-import { useReactToPrint } from "react-to-print";
+import { DataTable } from "@/components/data-table";
 import {
   translateCurrency,
   translateDate,
   translateNumber,
 } from "@/utils/helpers";
 
-export function ExpensesTable({ expenses }) {
-  const contentRef = useRef(null);
-  const reactToPrintFn = useReactToPrint({ contentRef });
-
-  return (
-    <>
-      <Button onClick={reactToPrintFn}>Print Data</Button>
-      <div className="overflow-x-auto md:rounded-md">
-        <table
-          className="w-full rounded-md border border-primary md:border-secondary text-xs md:text-base"
-          ref={contentRef}
-        >
-          <thead>
-            <tr className="text-center bg-input">
-              <th
-                scope="col"
-                className="p-2 border-r border-primary md:border-secondary"
-              >
-                সি. নং
-              </th>
-              <th
-                scope="col"
-                className="p-2 border-r border-primary md:border-secondary"
-              >
-                নাম
-              </th>
-              <th
-                scope="col"
-                className="p-2 border-r border-primary md:border-secondary"
-              >
-                তারিখ
-              </th>
-              <th
-                scope="col"
-                className="p-2 border-r border-primary md:border-secondary"
-              >
-                পরিমাণ
-              </th>
-              <th
-                scope="col"
-                className="p-2 border-r border-primary md:border-secondary"
-              >
-                যোগ করেছেন
-              </th>
-              <th scope="col" className="p-2 md:border-0 print:hidden"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {expenses?.map((expense, index) => (
-              <tr key={index} className="text-center even:bg-secondary">
-                <td className="p-2 border-r border-primary md:border-secondary">
-                  {translateNumber(index + 1)}
-                </td>
-                <th
-                  scope="row"
-                  className="p-2 border-r border-primary md:border-secondary"
-                >
-                  {expense?.name}
-                </th>
-                <td className="p-2 border-r border-primary md:border-secondary">
-                  {translateDate(expense.createdAt)}
-                </td>
-                <td className="p-2 border-r border-primary md:border-secondary">
-                  {translateCurrency(expense?.amount)}
-                </td>
-                <td className="p-2 border-r border-primary md:border-secondary">
-                  {expense.addedBy?.name}
-                </td>
-                <td className="p-2 md:border-0 print:hidden">
-                  <Link href="" className="underline text-cyan-800 font-bold">
-                    বিস্তারিত
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+const columns = [
+  {
+    header: "সি. নং",
+    accessorKey: "index",
+    cell: (_, index) => translateNumber(index + 1),
+  },
+  {
+    header: "নাম",
+    accessorKey: "name",
+  },
+  {
+    header: "তারিখ",
+    accessorKey: "createdAt",
+    cell: (expense) => translateDate(expense.createdAt),
+  },
+  {
+    header: "পরিমাণ",
+    accessorKey: "amount",
+    cell: (expense) => translateCurrency(expense.amount),
+  },
+  {
+    header: "যোগ করেছেন",
+    accessorKey: "addedBy.name",
+  },
+  {
+    header: "",
+    accessorKey: "id",
+    cell: () => (
+      <div className="print:hidden">
+        <Link href="" className="underline text-cyan-800 font-bold">
+          বিস্তারিত
+        </Link>
       </div>
-    </>
-  );
+    ),
+  },
+];
+
+export function ExpensesTable({ expenses }) {
+  return <DataTable columns={columns} data={expenses} printable withAction />;
 }
