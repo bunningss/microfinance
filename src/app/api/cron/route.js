fc9a9769f0cdbd18a3d17197f23711c52d2eb6b1
tayef mahmud <mahmudovi183@gmail.com>
2024-11-30 14:20:40 +0600

    default cron setup

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -1,9 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    return NextResponse.json({ msg: "Hello" }, { status: 200 });
-  } catch (err) {
-    return NextResponse.json({ msg: err.message }, { status: 400 });
-  }
+  return NextResponse.json({ ok: true });
 }
